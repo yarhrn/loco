7e8af3393a03ea5c40e9e09e0ca5272d9d6fb168
Dmitriy <mayboroda.de@yandex.ru>
2018-08-07 14:18:10 +0300

    #time 10m Fix PR issues

--- core/src/test/scala/loco/repository/InMemoryRepositoryTest.scala
+++ core/src/test/scala/loco/repository/InMemoryRepositoryTest.scala
@@ -5,23 +5,24 @@ import java.time.Instant
 import cats.data.{EitherT, NonEmptyList}
 import cats.effect.IO
 import loco.domain._
-import org.scalatest.{FunSuite, Matchers}
+import org.scalatest.{FlatSpec, Matchers}
 
-class InMemoryRepositoryTest extends FunSuite with Matchers with TestDomainData {
+class InMemoryRepositoryTest extends FlatSpec with Matchers with TestDomainData {
 
-  type Forum[A] = EitherT[IO, Throwable, A]
+  trait ctx {
+    val repository = new InMemoryRepository[Forum, ForumPostEvent]()
+  }
 
-  val repository = new InMemoryRepository[Forum, ForumPostEvent]()
+  type Forum[A] = IO[A]
 
-  test("Should store events") {
+  "InMemoryRepository" should "store events" in new ctx {
 
     val metaEvent = getMetaEvent(AggregateVersion(1), "Hello world", Users.john)
 
     val result = repository.saveEvents(NonEmptyList.one(metaEvent))
 
-    val either = result.value.unsafeRunSync()
-    either.isLeft shouldBe false
-    either.isRight shouldBe true
+    val either = result.unsafeRunSync()
+    either.leftSide shouldBe ()
   }
 
   def getMetaEvent(version: AggregateVersion[ForumPostEvent], content: String, author: User):MetaEvent[ForumPostEvent] = {
