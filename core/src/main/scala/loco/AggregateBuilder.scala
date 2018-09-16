ccb61397a792511db573ef197ab94f7505bdd0a1
yaroslavh <yaroslavh@wix.com>
2018-09-16 14:34:36 +0300

    #10| aggregate builder scaladoc

--- core/src/main/scala/loco/AggregateBuilder.scala
+++ core/src/main/scala/loco/AggregateBuilder.scala
@@ -1,14 +1,22 @@
 package loco
 
-import cats.ContravariantMonoidal
 import loco.domain.{Aggregate, AggregateId, Event, MetaEvent}
 
+/**
+  * An AggregateBuilder[A,E] describes a way how to build aggregate of type A from events of type E.
+  *
+  * @tparam A type of aggregate build from events of type E
+  * @tparam E type of events
+  */
 trait AggregateBuilder[A <: Aggregate[E], E <: Event] {
+  /**
+    * Builds 'empty' aggregate from given `id`
+    */
   def empty(id: AggregateId[E]): A
 
-  def apply(aggregate: A, metaEvent: MetaEvent[E]): A
-}
 
-object AggregateBuilder{
-  ContravariantMonoidal
+  /**
+    * Returns new aggregate as a the result of applying given `metaEvent` on give `aggregate`
+    */
+  def apply(aggregate: A, metaEvent: MetaEvent[E]): A
 }
