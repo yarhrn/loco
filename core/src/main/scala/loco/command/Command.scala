e875b3dd29f52956bb748510abeb283649166345
yaroslavh <yaroslavh@wix.com>
2018-10-04 14:21:22 +0300

    fixes #22

--- core/src/main/scala/loco/command/Command.scala
+++ core/src/main/scala/loco/command/Command.scala
@@ -4,10 +4,10 @@ import cats.Id
 import loco.domain.{Aggregate, Event}
 
 trait Command[F[_], E <: Event, A <: Aggregate[E], R] {
-  def events(a: Aggregate[E]): F[Either[(Throwable, List[E]), (List[E], R)]]
+  def events(a: A): F[Either[(Throwable, List[E]), (List[E], R)]]
 }
 
 trait PureCommand[E <: Event, A <: Aggregate[E], R] extends Command[Id,E,A,R]{
-  def events(a: Aggregate[E]): Either[(Throwable, List[E]), (List[E], R)]
+  def events(a: A): Either[(Throwable, List[E]), (List[E], R)]
 }
 
