87a7b46e183453dc532cdff2c8310db160d0a46b
yaroslavh <yaroslavh@wix.com>
2018-10-13 22:59:42 +0300

    #23| poly thread partitioning

--- core/src/main/scala/loco/PartitionedEventSource.scala
+++ core/src/main/scala/loco/PartitionedEventSource.scala
@@ -10,7 +10,7 @@ import loco.domain.{Aggregate, AggregateId, AggregateVersion, Event}
 
 object PartitionedEventSource {
 
-  def partition[F[_] : Concurrent, E <: Event, A <: Aggregate[E]](es: EventSourcing[F, E, A], queue: Queue[F, F[Unit]]) = {
+  def partition0[F[_] : Concurrent, E <: Event, A <: Aggregate[E]](es: EventSourcing[F, E, A], queue: Queue[F, F[Unit]]): F[EventSourcing[F, E, A]] = {
 
 
     val ess = new EventSourcing[F, E, A] {
@@ -43,10 +43,37 @@ object PartitionedEventSource {
       override def fetchMetaAggregate(id: AggregateId[E]) = es.fetchMetaAggregate(id)
     }
 
-    for{
-      _ <- queue.dequeue.flatMap{
+    for {
+      _ <- queue.dequeue.flatMap {
         action => fs2.Stream.eval(action)
       }.compile.drain
     } yield ess
+  }
+
+  def partition[F[_] : Concurrent, E <: Event, A <: Aggregate[E]](es: EventSourcing[F, E, A], partitionNumber: Int, queue: F[Queue[F, F[Unit]]]) = {
+    for {
+      partitions <- queue.replicateA(partitionNumber).map {
+        queues =>
+          queues.zipWithIndex.map(_.swap).toMap
+      }.map(_.mapValues(queue => partition0[F, E, A](es, queue))).map {
+        _.toList.map {
+          case (p, es) => es.map((p, _))
+        }.sequence.map(_.toMap)
+      }.flatten
+    } yield new EventSourcing[F, E, A] {
+
+      override def saveEvents(events: NonEmptyList[E], id: AggregateId[E], lastKnownVersion: AggregateVersion[E]) = {
+        partitions(id.hashCode() % partitionNumber)
+          .saveEvents(events, id, lastKnownVersion)
+      }
+
+
+      override def executeCommand[R](id: AggregateId[E], command: Command[F, E, A, R]) = {
+        partitions(id.hashCode() % partitionNumber)
+          .executeCommand(id, command)
+      }
+
+      override def fetchMetaAggregate(id: AggregateId[E]) = es.fetchMetaAggregate(id)
+    }
   }
 }
