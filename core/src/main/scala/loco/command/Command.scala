3cb0246aa01ec827dd5af7bab51a3ebe6bf9b06e
yaroslavh <yaroslavh@wix.com>
2018-12-22 13:35:25 +0200

    remove aggregate generic from command result

--- core/src/main/scala/loco/command/Command.scala
+++ core/src/main/scala/loco/command/Command.scala
@@ -4,23 +4,23 @@ import cats.data.NonEmptyList
 import loco.domain.{Aggregate, Event}
 
 trait Command[F[_], E <: Event, A <: Aggregate[E], R] {
-  def events(a: A): F[CommandResult[E, A, R]]
+  def events(a: A): F[CommandResult[E, R]]
 }
 
-sealed trait CommandResult[E <: Event, A <: Aggregate[E], R]
+sealed trait CommandResult[E <: Event, R]
 
-case class FailedCommand[E <: Event, A <: Aggregate[E], R](th: Throwable, events: List[E] = List()) extends CommandResult[E, A, R]
+case class FailedCommand[E <: Event, R](th: Throwable, events: List[E] = List()) extends CommandResult[E, R]
 
-case class SuccessCommand[E <: Event, A <: Aggregate[E], R](result: R, events: NonEmptyList[E]) extends CommandResult[E, A, R]
+case class SuccessCommand[E <: Event, R](result: R, events: NonEmptyList[E]) extends CommandResult[E, R]
 
-case class SuccessUnitCommand[E <: Event, A <: Aggregate[E], R](events: NonEmptyList[E]) extends CommandResult[E, A, R]
+case class SuccessUnitCommand[E <: Event, R](events: NonEmptyList[E]) extends CommandResult[E, R]
 
 object CommandResult {
-  def success[E <: Event, A <: Aggregate[E], R](e: E, tail: E*): CommandResult[E, A, R] = {
-    SuccessUnitCommand[E, A, R](NonEmptyList.of(e, tail: _*))
+  def success[E <: Event, R](e: E, tail: E*): CommandResult[E, R] = {
+    SuccessUnitCommand[E, R](NonEmptyList.of(e, tail: _*))
   }
 
-  def success[E <: Event, A <: Aggregate[E], R](r: R, e: E, tail: E*): CommandResult[E, A, R] = {
+  def success[E <: Event, R](r: R, e: E, tail: E*): CommandResult[E, R] = {
     SuccessCommand(r, NonEmptyList.of[E](e, tail: _*))
   }
 
