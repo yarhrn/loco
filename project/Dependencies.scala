5b6f7916f5aac2f561ddf119e6d4b1045aa639c5
Scala Steward <scala_steward@virtuslab.com>
2023-01-26 00:37:22 +0000

    Update cats-effect to 3.3.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,12 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0" % Test
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" % Test
 
-  lazy val postgresql ="org.postgresql" % "postgresql" % "42.2.8" % Test
-  lazy val embeddedPostgresql ="com.dimafeng" %% "testcontainers-scala-postgresql" % "0.39.12" % "test"
+  lazy val postgresql = "org.postgresql" % "postgresql" % "42.2.8" % Test
+  lazy val embeddedPostgresql = "com.dimafeng" %% "testcontainers-scala-postgresql" % "0.39.12" % "test"
 
   lazy val doobieCore = "org.tpolecat" %% "doobie-core" % "1.0.0-RC2"
 
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.4"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.14"
   lazy val catsEffectStd = "org.typelevel" %% "cats-effect-std" % "3.3.11"
   lazy val fs2Core = "co.fs2" %% "fs2-core" % "3.2.4"
   lazy val fs2Reactive = "co.fs2" %% "fs2-reactive-streams" % "2.5.6"
