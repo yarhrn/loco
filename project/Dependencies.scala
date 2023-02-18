41de374a1a5e9869a8a219e6fba8a420caf4eb4f
Scala Steward <scala_steward@virtuslab.com>
2023-02-18 21:54:05 +0000

    Revert commit(s) e87e60e

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" % Test
 
   lazy val postgresql = "org.postgresql" % "postgresql" % "42.2.8" % Test
-  lazy val embeddedPostgresql = "com.dimafeng" %% "testcontainers-scala-postgresql" % "0.40.12" % "test"
+  lazy val embeddedPostgresql = "com.dimafeng" %% "testcontainers-scala-postgresql" % "0.39.12" % "test"
 
   lazy val doobieCore = "org.tpolecat" %% "doobie-core" % "1.0.0-RC2"
 
