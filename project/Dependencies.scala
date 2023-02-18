7a47122543c6332837437059a274b303ff9f81e2
Scala Steward <scala_steward@virtuslab.com>
2023-02-18 21:54:14 +0000

    Revert commit(s) 21b5beb

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0" % Test
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" % Test
 
-  lazy val postgresql = "org.postgresql" % "postgresql" % "42.2.8" % Test
-  lazy val embeddedPostgresql = "com.dimafeng" %% "testcontainers-scala-postgresql" % "0.39.12" % "test"
+  lazy val postgresql ="org.postgresql" % "postgresql" % "42.2.8" % Test
+  lazy val embeddedPostgresql ="com.dimafeng" %% "testcontainers-scala-postgresql" % "0.39.12" % "test"
 
   lazy val doobieCore = "org.tpolecat" %% "doobie-core" % "1.0.0-RC2"
 
@@ -14,8 +14,8 @@ object Dependencies {
   lazy val fs2Core = "co.fs2" %% "fs2-core" % "3.2.4"
   lazy val fs2Reactive = "co.fs2" %% "fs2-reactive-streams" % "2.5.6"
 
-  lazy val jsoniter = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.1.15"
-  lazy val jsoniterMacros = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.1.15"
+  lazy val jsoniter = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.1.2"
+  lazy val jsoniterMacros = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.1.2"
 
   lazy val mongodbReactiveStreams = "org.mongodb" % "mongodb-driver-reactivestreams" % "1.13.0"
 
