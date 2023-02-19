db29dec25007e077f23a24154e76a2ff5bf8e9ee
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-02-19 18:27:25 +0100

    Update postgresql to 42.5.4 (#53)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0" % Test
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" % Test
 
-  lazy val postgresql = "org.postgresql" % "postgresql" % "42.2.27" % Test
+  lazy val postgresql = "org.postgresql" % "postgresql" % "42.5.4" % Test
   lazy val embeddedPostgresql = "com.dimafeng" %% "testcontainers-scala-postgresql" % "0.39.12" % "test"
 
   lazy val doobieCore = "org.tpolecat" %% "doobie-core" % "1.0.0-RC2"
