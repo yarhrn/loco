9a20238f8ea31adbe2873761b5d0a54dee53999b
Scala Steward <scala_steward@virtuslab.com>
2025-09-05 22:13:46 +0000

    Update fs2-core to 3.12.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.6.0"
   lazy val catsEffectStd = "org.typelevel" %% "cats-effect-std" % "3.6.0"
-  lazy val fs2Core = "co.fs2" %% "fs2-core" % "3.12.0"
+  lazy val fs2Core = "co.fs2" %% "fs2-core" % "3.12.2"
 
   lazy val jsoniter = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core" % "2.30.7"
   lazy val jsoniterMacros = "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.30.7"
