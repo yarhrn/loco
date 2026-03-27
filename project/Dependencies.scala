f2e36e93e2fcf639919fd7d659e66fa0a6f46bf7
Scala Steward <scala_steward@virtuslab.com>
2026-03-27 06:07:52 +0000

    Update scalatest to 3.2.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19" % Test
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.20" % Test
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "6.0.0" % Test
 
   lazy val postgresql = "org.postgresql" % "postgresql" % "42.7.3" % Test
